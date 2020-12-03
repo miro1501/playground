93bd7e0e286439dc138e2742be9ac96cd4b7173d
miro1501 <mirosrednoselec@yahoo.com>
2020-12-03 22:40:46 +0100

    merge one sorted list bugfix

--- leetcode/mergeonesortedlists.py
+++ leetcode/mergeonesortedlists.py
@@ -1,15 +1,15 @@
-nums = [1,1,2,2,3,3,4]
+#input [1,1,2,2,3,3,4]
+#output [1,2,3,4]
+#nums = [1,1,2,2,3,3,4]
+nums = [1, 1, 1, 2, 2, 2, 3, 3, 3]
 next_index = 1
 index = 0
 while index < len(nums):
     if nums[index] != nums[next_index]:
-        nums[next_index] = nums[index]
-        nums.pop(index)        
-        index = index + 1
+        next_index = next_index + 1    
+        index = index + 1            
     else:
         nums.pop(index)  
-        index = index + 1          
-    next_index = next_index + 1    
-    index = index + 1
-    print("-> nums ", nums)
+    if next_index >= len(nums):
+        break
 print(nums)
--- leetcode/mergetwosortedlist.py
+++ leetcode/mergetwosortedlist.py
@@ -1,8 +1,20 @@
+def sort_two_list(nums):
+    next_index = 1
+    index = 0
+    while index < len(nums):
+        if nums[index] != nums[next_index]:
+            next_index = next_index + 1    
+            index = index + 1            
+        else:
+            nums.pop(index)  
+        if next_index >= len(nums):
+            break
+        return nums
+    
 l1 = [1, 1, 2, 3]
 l2 = [1, 2, 2, 3, 3]
-new_list = l1 + l2
-print(sorted(new_list))
+new_num_list = l1 + l2
 
-
-def sort_two_list(list1, list2):
-    
+new_sorted_num_list = sorted(new_num_list)
+#print(sorted(new_sorted_num_list)) 
+print(sort_two_list(new_sorted_num_list))
